7b3affa84d740448f99aa68d1f90054f5d32dbde
AI Manga Reader Developer <developer@aimangareader.com>
2025-09-30 13:05:57 +0700

    Remove Latest Community Activity and Comments API - Clean up problematic features for better performance

--- app/(core)/manga-list/page.jsx
+++ app/(core)/manga-list/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import MangaReadHistory from '../../Components/MangaListComponents/MangaReadHistory';
-import LatestActivityComments from '../../Components/MangaListComponents/LatestActivityComments';
 import MangaCard from '../../Components/MangaListComponents/MangaCard';
 import AsideComponent from '../../Components/MangaListComponents/AsideComponent';
 import SliderComponent from '../../Components/MangaListComponents/SliderComponent';
@@ -11,9 +10,7 @@ const MangaList = () => {
       <div className="w-full h-fit">
         <SliderComponent />
       </div>
-      <div className="hidden px-6 xl:px-16   lg:block">
-        <LatestActivityComments />
-      </div>
+      {/* Removed Latest Community Activity component */}
       <div className="flex flex-col-reverse md:gap-3  md:flex-row mt-6 ">
         <div className="flex-1 px-2 md:pl-6 xl:pl-16 sm:px-0 ">
           <MangaCard />
